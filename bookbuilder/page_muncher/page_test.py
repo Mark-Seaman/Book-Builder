18b5c14c11000da576ea16908b019de0aab50d0b
Mark Seaman <Mark.Seaman@imac.net>
2020-11-11 15:52:39 -0700

    Lesson 34

--- bookbuilder/page_muncher/page_test.py
+++ bookbuilder/page_muncher/page_test.py
@@ -12,46 +12,46 @@ def open_browser():
     return webdriver.Chrome(options=options)
 
 
-def test_selenium():
-    browser = open_browser()
-    browser.get('http://shrinking-world.com/course/cs350')
-    browser.quit()
-
-
-def test_selenium_text():
-
-    def test_page_header(browser):
-        header_title = browser.find_element_by_css_selector('header h1')
-        header_title = header_title.get_attribute("innerHTML")
-        expected = '<a href="/course/cs350">UNC CS 350</a>'
-        assert expected in header_title
-
-    def test_links(browser):
-        links = browser.find_elements_by_css_selector('li a[href]')
-        links = [link.get_attribute("innerHTML") for link in links]
-        # print(links)
-        assert len(links) == 12
-
-    def test_html(browser):
-        assert len(browser.page_source) == 59478
-
-    def test_text(browser):
-        body_text = browser.find_element_by_tag_name('body').text
-        assert len(body_text) == 682
-
-    # Open the browser
-    browser = open_browser()
-
-    # Get the web page
-    url = 'http://shrinking-world.com/course/cs350/lesson/Index'
-    browser.get(url)
-
-    # Verify the page content
-    test_page_header(browser)
-    test_links(browser)
-    test_html(browser)
-    test_text(browser)
-
-    # Close the browser
-    browser.quit()
-
+# def test_selenium():
+#     browser = open_browser()
+#     browser.get('http://shrinking-world.com/course/cs350')
+#     browser.quit()
+#
+#
+# def test_selenium_text():
+#
+#     def test_page_header(browser):
+#         header_title = browser.find_element_by_css_selector('header h1')
+#         header_title = header_title.get_attribute("innerHTML")
+#         expected = '<a href="/course/cs350">UNC CS 350</a>'
+#         assert expected in header_title
+#
+#     def test_links(browser):
+#         links = browser.find_elements_by_css_selector('li a[href]')
+#         links = [link.get_attribute("innerHTML") for link in links]
+#         # print(links)
+#         assert len(links) == 12
+#
+#     def test_html(browser):
+#         assert len(browser.page_source) == 59478
+#
+#     def test_text(browser):
+#         body_text = browser.find_element_by_tag_name('body').text
+#         assert len(body_text) == 682
+#
+#     # Open the browser
+#     browser = open_browser()
+#
+#     # Get the web page
+#     url = 'http://shrinking-world.com/course/cs350/lesson/Index'
+#     browser.get(url)
+#
+#     # Verify the page content
+#     test_page_header(browser)
+#     test_links(browser)
+#     test_html(browser)
+#     test_text(browser)
+#
+#     # Close the browser
+#     browser.quit()
+#
--- bookbuilder/page_muncher/quick_test.py
+++ bookbuilder/page_muncher/quick_test.py
@@ -1,9 +1,11 @@
 from requests import get
+# from book.models import Book
 
 
 def test_quick():
-    test_book_list()
-    # test_pytest()
+    # test_book_list()
+    # test_book_list()
+    test_pytest()
 
 
 def test_pytest():
@@ -11,12 +13,17 @@ def test_pytest():
     assert 1 == 1
 
 
-def test_book_list():
+def test_book_view():
     response = get('http://127.0.0.1:8002/book/')
     assert response.status_code == 200
     text = response.content.decode('utf-8')
     assert '<a href="/">Book Builder</a>' in text
     print(text)
+
+
+# def test_book_list():
+#     for book in Book.objects.all():
+#         print(book.author.name, ' -- ', book.title)
 
 
 def test_shrinking_world():
